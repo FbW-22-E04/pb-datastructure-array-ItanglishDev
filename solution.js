d983f8611bc56c1b0771dbf6552dddd82884f387
chris <chrishethertonweb@gmail.com>
2022-07-07 11:54:35 +0200

    exercise done

---
+++ solution.js
@@ -0,0 +1,8 @@
+const myArray = ["1", "js", "3", "6", "problem"];
+console.log(myArray);
+
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+myArray.splice(3, 1, "fourth element");
+console.log(myArray);
